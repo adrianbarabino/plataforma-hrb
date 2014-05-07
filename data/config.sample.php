616fbd734216f0c3c9a1b97e9ea25c7b5e9d7d50
Adrian Barabino <adrian.barabino@gmail.com>
2014-05-07 15:44:25 -0300

    added config.sample.php

---
+++ data/config.sample.php
@@ -0,0 +1,19 @@
+<?php 
+
+
+// Configuration file
+
+
+$config = array(
+	"db_user" => 'username',
+	"db_pass" => "password",
+	"db_host" => "localhost",
+	"db_port" => "3306",
+	"db_name" => "database",
+	"web_title" => "Testing",
+	"theme" => "2014",
+	"admin_mail" => "email@provider.tld"
+	);
+
+
+?>
